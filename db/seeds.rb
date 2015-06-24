71bce714d743bf1f5635c0d1f31ded059b184ffc
Edinunzio <e.dinunzio@gmail.com>
2015-06-24 17:43:08 -0400

    adding seed data, light styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+factories = Factory.create([{ name: 'H & H Robotics', email: 'house@test.com', address1: '123 New Vegas Strip', city: 'New Vegas', state: 'NV', zipcode: '12345', tags: 'robots, electronics, scrap metal'},
+                            { name: 'Factory A', email: 'factorya@test.com', address1: '23 New Factory St', city: 'New York', state: 'NY', zipcode: '44345'},
+                            { name: 'The Mill', email: 'david@lynch.com', address1: '12 Mullholland Dr', city: 'Los Angeles', state: 'CA', zipcode: '55432'},
+                            { name: 'Silver Rush', email: 'silver@test.com', address1: '332 Atlantic Ave', city: 'Brooklyn', state: 'NY', zipcode: '65364', tags: 'jewelry, silver'}
+                           ])
